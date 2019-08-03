00cc4f181640eb10aaccabcdd41228469b7a8b5b
Taylor Gorman <me@taylorpatrickgorman.com>
2019-08-02 19:20:34 -0500

    👷‍♂️ modify create react app

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './index.css'
+import UserInput from '../UserInput'
+
+
+export default function () {
+
+  return (
+
+    <div className="App">
+      <UserInput />
+    </div>
+
+  )
+
+}
