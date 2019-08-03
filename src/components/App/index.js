3e2e508b711f3d610b1340bcfbe015e3a75f7fbb
Taylor Gorman <me@taylorpatrickgorman.com>
2019-08-03 16:33:30 -0500

    👷‍♂️ style guide

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Calculator from '../Calculator'
+import StyleGuide from '../StyleGuide'
 
 
 export default function () {
@@ -10,6 +11,7 @@ export default function () {
 
     <Router>
       <Route exact path="/" component={ Calculator } />
+      <Route exact path="/style-guide" component={ StyleGuide } />
     </Router>
 
   )
